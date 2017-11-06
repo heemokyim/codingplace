748e794cfe4c048254580c4e3fb077f82b211250
JW <anylee2142@gmail.com>
2017-11-06 21:50:29 +0900

    missing users.js added

---
+++ mongoDB/workspace/3_project/src/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: String
+});
+
+const User = mongoose.model('user', UserSchema);
+// 1. if not 'user' in DB, make 'user' collection
+// 2. 'user' collection follows UserSchema
+// User == entire collection
+
+module.exports=User;

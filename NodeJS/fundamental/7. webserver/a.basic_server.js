17bec4052eab3d48150a655bb0488bcaee498628
Anylee2142 <Anylee2142@gmail.com>
2017-11-18 21:32:21 +0900

    rollback

--- NodeJS/fundamental/7. webserver/a.basic_server.js
+++ NodeJS/fundamental/7. webserver/a.basic_server.js
@@ -1,18 +1,14 @@
 var http = require('http');
+var mongoose = require('mongoose');
 
-// argument is callback function
-// callback() by V8 after http.createServer() by OS
-
-// whenever req happens,
-// below is executed with req, res 
 http.createServer(function(req, res){
     // status code = 200, Content-Type = text/plain
     res.writeHead(200,{'Content-Type':'text/plain'});
     // res.end = sending only this one
     res.end('Hello world\n');
 
-    // Port number, IP Address
-}).listen(1337,'127.0.0.1');
+    console.log(req)
 
-// 1. res is writable stream
-// 2. F12 when debugging in chrome browser
+
+
+}).listen(1337,'127.0.0.1');

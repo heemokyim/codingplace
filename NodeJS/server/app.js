1e470c8f27b3629a323cc8068d30502463b86e26
Anylee2142 <Anylee2142@gmail.com>
2017-11-18 16:48:39 +0900

    fixed

--- NodeJS/server/app.js
+++ NodeJS/server/app.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var app = express();
-var setupController = require('./controllers/setupController');
 var apiController = require('./controllers/apiController');
 
 var port = process.env.PORT || 3000;
 
-mongoose.Promise=global.Promise;
-
-setupController(app);
 apiController(app);
 
-app.listen(port);
+app.listen(port,()=>{
+  console.log('Server running on '+port)
+});

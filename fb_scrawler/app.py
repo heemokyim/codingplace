3db1d24f9b2a36ec182193f2f5060e2f0ccbe6f6
Anylee2142 <Anylee2142@gmail.com>
2017-11-26 20:17:56 +0900

    FB_cralwer frame

---
+++ fb_scrawler/app.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome('./chromedriver')
+
+try:
+    driver.get('https://www.facebook.com')
+
+    ID = driver.find_element_by_name('email')
+    PW = driver.find_element_by_name('pass')
+
+    ID.send_keys('anylee77@naver.com')
+    with open('/home/ej/older/password','r') as file:
+        PW.send_keys(file.read())
+        # PW.send_keys(Keys.RETURN)
+
+    time.sleep(1)
+
+    driver.switch_to_alert()
+
+    
+
+    input()
+
+except Exception as e:
+    print(e)
+
+finally:
+    driver.quit()

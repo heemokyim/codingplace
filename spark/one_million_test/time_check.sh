383d6af6c6f198b1eab9f50aa42ff9b9b9d6d7c5
ej <Anylee2142@gmail.com>
2018-04-30 14:18:52 +0900

    180430 Created new repo for pyspark

--- spark/one_million_test/time_check.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-start_time="$(date -u +%s)"
-spark-submit MovieSimilarities1M.py 260
-end_time="$(date -u +%s)"
-
-elapsed="$(($end_time-$start_time))"
-echo "Total of $elapsed seconds elapsed for process"
